de6260d10f60317418eb0fea80708d78234d8f3f
Ann-Kathrin Barth <ann-kathrin.barth@student.hs-rm.de>
2025-01-29 10:26:47 +0100

    feat: inventory button to open and close inventory

--- Assets/_Course Library/Scripts/Grelody/Entities/Snowmen/SnowmanInventoryManager.cs
+++ Assets/_Course Library/Scripts/Grelody/Entities/Snowmen/SnowmanInventoryManager.cs
@@ -1,9 +1,12 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SnowmanInventoryManager : MonoBehaviour
 {
+    public GameObject inventory; // Panel containing the grid
     public Transform inventoryGrid; // Parent object for the snowmen in the inventory
+    public Button openInventoryButton; // Opens and closes inventory
     public Vector3 gridStartPosition = Vector3.zero;
     public float rowItemSpacing = 30.0f; // Horizontal gap between snowmen
     public float columnItemSpacing = 20.0f; // Vertical gap between snowmen
@@ -11,6 +14,12 @@ public class SnowmanInventoryManager : MonoBehaviour
 
     private int gridIndex = 0;
     private List<SnowmanMelody> savedSnowmen = new List<SnowmanMelody>();
+
+
+    public void Start()
+    {
+        openInventoryButton.onClick.AddListener(OpenCloseInventory);
+    }
 
     // Saves the snowman to the inventory
     public void SaveSnowman(SnowmanMelody snowmanMelody)
@@ -30,5 +39,11 @@ public class SnowmanInventoryManager : MonoBehaviour
         int column = gridIndex % columns;
         gridIndex++;
         return gridStartPosition + new Vector3(column * columnItemSpacing, -row * rowItemSpacing, 0);
+    }
+
+    // Shows inventory if not visible and hides inventory if visible
+    private void OpenCloseInventory()
+    {
+        inventory.SetActive(!inventory.activeSelf);
     }
 }
